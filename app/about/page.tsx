c6d523ddedcb36dc58285c975d71f45068e939ca
suman <sumankumarghosh94@gmail.com>
2024-07-13 00:58:16 +0530

    update app

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,46 @@
+import IconCloud from "@/components/custom/icon-cloud";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
 const AboutPage = () => {
-    return ( 
-        <div>
-            About
-        </div>
-     );
+  return (
+    <div className="flex flex-col items-center justify-center pt-40">
+      <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
+        <IconCloud iconSlugs={slugs} />
+      </div>
+    </div>
+  );
 }
- 
+
 export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { TypewriterEffect } from "@/components/custom/typewriter";
+import { ModeToggle } from "@/components/modeToggle";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -9,14 +12,40 @@ export default function Home() {
     router.push("/about")
   }
 
+  const words = [
+    {
+      text: "Suman's"
+    },
+    {
+      text: "Profile",
+      classname: "text-blue-500 dark:text-cyan-400"
+    }
+  ]
+
   return (
-     <div>
-      <div className="flex flex-col items-center justify-center text-8xl text-white">
-      Suman
-     </div>
-      <button onClick={onClick}>
-        go to about
-      </button>
+     <div className="flex flex-col space-y-4">
+      <div className="flex p-4 items-end w-full">
+      <ModeToggle />
+      </div>
+      <div className="flex flex-col p-40 space-y-4">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <h1 className="flex text-8xl text-black dark:text-white">
+            Welcome to
+          </h1>
+          <div className="pt-2">
+          <TypewriterEffect words={words} className="pt-4"/>
+          </div>
+          <div className="pt-4">
+          <button onClick={onClick} className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              About me
+            </span>
+          </button>
+          </div>
+        </div>
+      </div>
+      
      </div>
   );
 }
